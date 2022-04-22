ff30c8b69a07962205143375345fea4878f06c39
Your Name <you@example.com>
2022-04-23 00:28:04 +0530

    Added: greeting controller to return JSON for different HTTP methods

--- src/main/java/com/example/greetingapplication/GreetingApplication.java
+++ src/main/java/com/example/greetingapplication/GreetingApplication.java
@@ -9,6 +9,7 @@ public class GreetingApplication {
     public static void main(String[] args) {
         SpringApplication.run(GreetingApplication.class, args);
         System.out.println("Hello");
+
     }
 
 }
