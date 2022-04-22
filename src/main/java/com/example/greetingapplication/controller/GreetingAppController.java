bd9a9288ab8e5543800e5dd2cc347e7614087c69
Your Name <you@example.com>
2022-04-23 00:41:44 +0530

    Added: used service layer to get simple greeting message

--- src/main/java/com/example/greetingapplication/controller/GreetingAppController.java
+++ src/main/java/com/example/greetingapplication/controller/GreetingAppController.java
@@ -3,16 +3,29 @@ package com.example.greetingapplication.controller;
 import java.util.concurrent.atomic.AtomicLong;
 
 import com.example.greetingapplication.model.Greeting;
-import org.springframework.web.bind.annotation.*;
+import com.example.greetingapplication.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
-@RequestMapping("/Hello")
 public class GreetingAppController {
     private static final String template="Hello %s";
     private static AtomicLong counter=new AtomicLong();
 
+    @Autowired
+    GreetingService greetingService;
+
     @GetMapping("/getGreeting")
-    public Greeting greeting(@RequestParam(value="name",defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam (value="name",defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),String.format(template, name));
     }
     @PostMapping("/postGreeting")
@@ -23,5 +36,8 @@ public class GreetingAppController {
     public Greeting sayHello(@PathVariable long counter,@RequestParam (value="content") String content) {
         return new Greeting(counter,String.format(template, content));
     }
+    @GetMapping("/getMessage")
+    public ResponseEntity<String> getMessage(){
+        return new ResponseEntity<String>(greetingService.getMessage(),HttpStatus.OK);
+    }
 }
-
---
+++ src/main/java/com/example/greetingapplication/service/GreetingService.java
@@ -0,0 +1,11 @@
+package com.example.greetingapplication.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+    public String getMessage() {
+        return "Hello World";
+    }
+}
+
