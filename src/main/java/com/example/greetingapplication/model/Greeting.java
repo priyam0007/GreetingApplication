b6095f5f74ef4ea91ca49db80c3085bfb7335052
Your Name <you@example.com>
2022-04-23 00:11:39 +0530

    Added: initial commit

---
+++ src/main/java/com/example/greetingapplication/model/Greeting.java
@@ -0,0 +1,30 @@
+package com.example.greetingapplication.model;
+
+import jdk.nashorn.internal.objects.annotations.Getter;
+
+public class Greeting {
+    private Long id;
+    private String content;
+    public Greeting(Long id, String content) {
+        super();
+        this.id = id;
+        this.content = content;
+    }
+    public Long getId() {
+
+        return id;
+    }
+    public void setId(Long id) {
+
+        this.id = id;
+    }
+    public String getContent() {
+
+        return content;
+    }
+    public void setContent(String content) {
+
+        this.content = content;
+    }
+}
+
